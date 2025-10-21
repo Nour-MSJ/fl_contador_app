4e3ccbe1f41e277e3f6fca290751c4e7d5f404b6
Nour <nour.sj05@gmail.com>
2025-10-21 11:12:43 +0200

    Segundo Commit - 3 Botones + 0 -

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,23 +1,89 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int contador = 0;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void reset() {
+    contador = 0;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const tamano30 = TextStyle(fontSize: 30);
+
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       appBar: AppBar(title: Text('HomeScreen'), elevation: 50.0),
       body: Center(
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Bocata lomo'),
-            SizedBox(height: 300.0, width: 300.0),
-            Text('YA'),
+            Text('Numero de clicks', style: tamano30),
+            SizedBox(height: 20),
+            Text('$contador', style: tamano30),
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomFloatingActionButtom(
+        incrementarFn: incrementar,
+        decrementarFn: decrementar,
+        resetFn: reset,
+      ),
+    );
+  }
+}
+
+class CustomFloatingActionButtom extends StatelessWidget {
+  final Function incrementarFn;
+  final Function decrementarFn;
+  final Function resetFn;
+
+  const CustomFloatingActionButtom({
+    super.key,
+    required this.incrementarFn,
+    required this.resetFn,
+    required this.decrementarFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          child: const Text('-1'),
+          onPressed: () => decrementarFn(),
+        ),
+        const SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Text('RESET'),
+          onPressed: () => resetFn(),
+        ),
+        const SizedBox(width: 20),
+        FloatingActionButton(
+          child: const Text('+1'),
+          onPressed: () => incrementarFn(),
+        ),
+      ],
     );
   }
 }
